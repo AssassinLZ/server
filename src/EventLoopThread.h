997ea987f2bacda6713f3cb07fa46f3d74c17d7d
Assassin <assassinl@live.com>
2023-12-22 22:28:35 +0800

    Changes to be committed: 	modified:   src/Channel.h 	modified:   src/Epoll.h 	modified:   src/EventLoopThread.h 	modified:   src/EventLoopThreadPool.h

--- src/EventLoopThread.h
+++ src/EventLoopThread.h
@@ -5,4 +5,28 @@
 #ifndef SERVER_EVENTLOOPTHREAD_H
 #define SERVER_EVENTLOOPTHREAD_H
 
+
+#include "EventLoop.h"
+#include "base/Condition.h"
+#include "base/MutexLock.h"
+#include "base/Thread.h"
+#include "base/noncopyable.h"
+
+class EventLoopThread : noncopyable{
+public:
+    EventLoopThread();
+    ~EventLoopThread();
+    EventLoop* startLoop();
+
+private:
+    void threadFunc();
+    Eventloop* loop_;
+    bool exiting_;
+    Thread thread_;
+    Mutexlock mutex_;
+    Condition cond_;
+};
+
+
+
 #endif //SERVER_EVENTLOOPTHREAD_H
--- src/EventLoopThreadPool.h
+++ src/EventLoopThreadPool.h
@@ -5,4 +5,30 @@
 #ifndef SERVER_EVENTLOOPTHREADPOOL_H
 #define SERVER_EVENTLOOPTHREADPOOL_H
 
+
+#include <memory>
+#include <vector>
+#include "EventLoopThread.h"
+#include "base/Logging.h"
+#include "base/noncopyable.h"
+
+class EventLoopThreadPoll : noncopyable {
+public:
+    EventLoopThreadPoll(EventLoop* baseeLoop, int numThreads);
+    ~EventLoopThreadPoll(){
+        LOG << "~EventLoopThreadPoll()";
+    }
+    void start()
+    EventLoop* getNextLoop();
+    
+private:
+    EventLoop* baseLoop_;
+    bool started_;
+    int numThreads_;
+    int next_;
+    std::vector<std::shared_ptr<EventLoopThread>> threads_;
+    std::vector<EventLoop*> loops_;
+};
+
+
 #endif //SERVER_EVENTLOOPTHREADPOOL_H
